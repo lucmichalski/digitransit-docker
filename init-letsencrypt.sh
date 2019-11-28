c3718dc2d276e716829271a63ed4d42f21bad918
Vlatko Vlahek <vlatko@prototyp.digital>
2019-11-28 21:22:13 +0100

    Update to files

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,10 +5,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(cityrouting.prototyp.digital api.cityrouting.prototyp.digital)
+domains=(cityrouting.prototyp.digital)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
+email="vlatko@prototyp.digital" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
