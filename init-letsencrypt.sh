3d4dbc100310129358705f0fa29c42aeb2b6b3da
Vlatko Vlahek <vlatko@prototyp.digital>
2019-11-29 13:27:37 +0100

    Updated init script a bit

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -12,7 +12,7 @@ email="vlatko@prototyp.digital" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+  read -p "Existing certificate data found. Continue and replace existing certificate? (y/N) " decision
   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
     exit
   fi
@@ -70,6 +70,8 @@ for domain in "${domains[@]}"; do
       --agree-tos \
       --force-renewal" certbot
   echo
+
+  echo "### Finished adding Let's Encrypt certificate for $domain ..."
 done
 
 echo "### Reloading nginx ..."
