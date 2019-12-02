10ddfff394522a9d9c875d71ac74d843de7f71ed
Vlatko Vlahek <vlatko@prototyp.digital>
2019-12-02 17:42:09 +0100

    Add pelias script

---
+++ init-pelias.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -x
+
+# set proper permissions. make sure the user matches your `DOCKER_USER` setting in `.env`
+chown 1000:1000 /code
+
+# clone repo
+cd /code
+git clone git@bitbucket.org:prototypdigital/digitransit-pelias.git
+cd docker
+
+# install pelias script
+ln -s "$(pwd)/pelias" /usr/local/bin/pelias
+
+# cwd
+cd projects/osijek
+
+# configure environment
+sed -i '/DATA_DIR/d' .env
+echo 'DATA_DIR=/data' >> .env
+
+# run build
+pelias compose pull
+pelias elastic start
+pelias elastic wait
+pelias elastic create
+pelias download all
+pelias prepare all
+pelias import all
+pelias compose up
+
+# optionally run tests
+pelias test run
