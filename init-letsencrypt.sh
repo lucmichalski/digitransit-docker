ead25d02c872c061d81301f71bd4e691498ac9a5
Vlatko Vlahek <vlatko@prototyp.digital>
2019-12-02 12:11:05 +0100

    Updated nginx conf

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(cityrouting.e-gpp.hr api.cityrouting.e-gpp.hr)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="vlatko@prototyp.digital" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing certificate data found. Continue and replace existing certificate? (y/N) " decision
